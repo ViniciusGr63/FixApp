0de3e4f4ad887b6a4f4e4e7aa20545f0e456d9d6
Trikest <130517609+Trikest@users.noreply.github.com>
2024-12-12 17:09:08 -0300

    Adicionando BD

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,12 +1,74 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _nomeController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _senhaController = TextEditingController();
+  final _enderecoController = TextEditingController();
+  final _telefoneController = TextEditingController();
+
+  Future<void> _registerUser() async {
+    final url = Uri.parse(
+        "https://ideiafix-back-end-824c.onrender.com/api"); // Substitua pelo URL da sua API
+
+    final body = {
+      "nome": _nomeController.text,
+      "email": _emailController.text,
+      "senha": _senhaController.text,
+      "fotoPerfil": "https://example.com/foto-perfil.jpg",
+      "Cliente": {
+        "endereco": _enderecoController.text,
+        "telefone": _telefoneController.text,
+      }
+    };
+
+    try {
+      print('Enviando requisição para $url');
+      print('Dados enviados: ${jsonEncode(body)}');
+
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(body),
+      );
+
+      print('Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 201) {
+        // Sucesso
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Usuário registrado com sucesso!')),
+        );
+      } else {
+        // Erro
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text('Erro ao registrar usuário: ${response.body}')),
+        );
+      }
+    } catch (e) {
+      // Erro de conexão ou outro problema
+      print('Erro de rede: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro de rede: $e')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffFC3F75),
+      backgroundColor: const Color(0xffFC3F75),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -14,136 +76,34 @@ class RegisterPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Column(
-                children: [
-                  Image.asset(
-                    'assets/images/fix_logo.png',
-                    height: 70,
-                  ),
-                  const SizedBox(height: 40),
-                  const Text(
-                    'Cadastre-se',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xffffffff),
-                    ),
-                  ),
-                  const SizedBox(height: 40),
-                ],
+              Image.asset(
+                'assets/images/fix_logo.png',
+                height: 70,
               ),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Nome completo:',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Color(0xff7a7a7a)),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Data de nascimento:',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Color(0xff646464)),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Endereço:',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Color(0xff646464)),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      '---------------:',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Color(0xff646464)),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      '---------------:',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Color(0xff646464)),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      ),
-                    ),
-                  ],
+              const SizedBox(height: 40),
+              const Text(
+                'Cadastre-se',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
               const SizedBox(height: 40),
+              _buildTextField("Nome completo", _nomeController),
+              _buildTextField("Email", _emailController),
+              _buildTextField("Senha", _senhaController, obscureText: true),
+              _buildTextField("Endereço", _enderecoController),
+              _buildTextField("Telefone", _telefoneController),
+              const SizedBox(height: 40),
               ElevatedButton(
-                onPressed: () {
-                  print('Entrar pressionado');
-                },
+                onPressed: _registerUser,
                 child: const Text(
                   'Cadastrar',
                   style: TextStyle(color: Colors.white),
                 ),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff7C7C7C),
+                  backgroundColor: const Color(0xff7C7C7C),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8),
                   ),
@@ -156,7 +116,7 @@ class RegisterPage extends StatelessWidget {
                   const Text(
                     'Já possui uma conta? ',
                     style: TextStyle(
-                      color: Color(0xff000000),
+                      color: Colors.black,
                     ),
                   ),
                   TextButton(
@@ -166,7 +126,7 @@ class RegisterPage extends StatelessWidget {
                     child: const Text(
                       'Entrar',
                       style: TextStyle(
-                        color: Color(0xffffffff),
+                        color: Colors.white,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
@@ -177,6 +137,35 @@ class RegisterPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller,
+      {bool obscureText = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$label:',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15.0,
+          ),
+        ),
+        TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: const InputDecoration(
+            labelStyle: TextStyle(color: Color(0xff7a7a7a)),
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            fillColor: Colors.white,
+            filled: true,
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
     );
   }
 }
